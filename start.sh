cc4dbfcd106fab23eb1d513fe16e3ddcb0b29476
Simone Flavio Paris <simone.flavio.paris@gmail.com>
2025-10-12 12:14:46 +0200

    Simplifies data volume mapping
    Simplifies the data volume mapping in `compose.yaml` by mapping the entire `data` directory instead of just `bot.db`.
    This change ensures that all files within the data directory are accessible to the container.
    
    Removes the explicit creation of the whisper directory as it is no longer needed with the simplified volume mapping.

--- start.sh
+++ start.sh
@@ -3,7 +3,6 @@
 cur_dir=$(dirname $(realpath "$0"))
 source .env
 
-mkdir -p "${WORK_DIR}/data/whisper"
 touch "${WORK_DIR}/data/bot.db"
 
 docker compose --project-directory ${cur_dir} build --pull
